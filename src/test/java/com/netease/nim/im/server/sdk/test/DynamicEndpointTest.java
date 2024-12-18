3fe8c426eb8d5736d6ade9c1463d486f65898646
cghk4455 <402264839@qq.com>
2024-12-18 16:35:42 +0800

    Update DynamicEndpointTest.java
    add case

--- src/test/java/com/netease/nim/im/server/sdk/test/DynamicEndpointTest.java
+++ src/test/java/com/netease/nim/im/server/sdk/test/DynamicEndpointTest.java
@@ -143,4 +143,57 @@ public class DynamicEndpointTest {
         Assert.assertEquals(endpoint2, list.get(0).endpoint);
     }
 
+    @Test
+    public void test6() {
+        String endpoint1 = "a";
+        String endpoint2 = "b";
+        String endpoint3 = "c";
+        String endpoint4 = "d";
+        String endpoint5 = "e";
+        DynamicEndpointSelector.Entry entry1 = new DynamicEndpointSelector.Entry(endpoint1, 0.9999, 60.0, true);
+        DynamicEndpointSelector.Entry entry2 = new DynamicEndpointSelector.Entry(endpoint2, 0.9999, 30.0, false);
+        DynamicEndpointSelector.Entry entry3 = new DynamicEndpointSelector.Entry(endpoint3, 0.9999, 80.0, false);
+        DynamicEndpointSelector.Entry entry4 = new DynamicEndpointSelector.Entry(endpoint4, 0.9999, 90.0, false);
+        DynamicEndpointSelector.Entry entry5 = new DynamicEndpointSelector.Entry(endpoint5, 0.9999, 50.0, false);
+
+        List<DynamicEndpointSelector.Entry> list = new ArrayList<>();
+        list.add(entry3);
+        list.add(entry1);
+        list.add(entry2);
+        list.add(entry5);
+        list.add(entry4);
+        Collections.shuffle(list);
+
+        Collections.sort(list);
+
+        Assert.assertEquals(endpoint2, list.get(0).endpoint);
+    }
+
+
+    @Test
+    public void test7() {
+        String endpoint1 = "a";
+        String endpoint2 = "b";
+        String endpoint3 = "c";
+        String endpoint4 = "d";
+        String endpoint5 = "e";
+        DynamicEndpointSelector.Entry entry1 = new DynamicEndpointSelector.Entry(endpoint1, 0.8, 60.0, true);
+        DynamicEndpointSelector.Entry entry2 = new DynamicEndpointSelector.Entry(endpoint2, 0.6, 30.0, false);
+        DynamicEndpointSelector.Entry entry3 = new DynamicEndpointSelector.Entry(endpoint3, 0.8, 80.0, false);
+        DynamicEndpointSelector.Entry entry4 = new DynamicEndpointSelector.Entry(endpoint4, 0.7, 90.0, false);
+        DynamicEndpointSelector.Entry entry5 = new DynamicEndpointSelector.Entry(endpoint5, 0.6, 50.0, false);
+
+        List<DynamicEndpointSelector.Entry> list = new ArrayList<>();
+        list.add(entry3);
+        list.add(entry1);
+        list.add(entry2);
+        list.add(entry5);
+        list.add(entry4);
+        Collections.shuffle(list);
+
+        Collections.sort(list);
+
+        Assert.assertEquals(endpoint1, list.get(0).endpoint);
+    }
+
 }
